122f56d2578d563bfd91fbd25cc9d2953ba395ee
WillyMoselhy <walid@almoselhy.com>
2017-08-16 14:38:09 +0200

    Added Disk and NIC reporting

--- Report-VMWareVMs.ps1
+++ Report-VMWareVMs.ps1
@@ -1,42 +1,73 @@
-﻿$Report = foreach ($VM in $VMWareVMs) {
-    $Object = New-Object -TypeName psobject
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "Name" -Value $VM.Name
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "FQDN" -Value $VM.Guest.HostName
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "OperatingSystem" -Value $VM.Guest.OSFullName
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "PowerState" -Value $VM.PowerState
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "CPUs" -Value $VM.NumCpu
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "Memory" -Value $VM.MemoryGB
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "VMWareToolsStatus" -Value $VM.ExtensionData.Guest.ToolsStatus
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "VMWareToolsVersion" -Value $VM.ExtensionData.Guest.ToolsVersion
+﻿<#
+    Created by Walid AlMoselhy - www.almoselhy.com
+    Available on GitHub: https://github.com/WillyMoselhy/PowerShell-Report-VMWare-VMs
+
+    Usage: Run after importing VMWare Core module with Prefix "VMWare" then connect to vSphere.
+
+    Produces three variable containing reports on VMs, Disks, and NICs.
+#>
+$VMWareVMs = Get-VMWareVM
+
+$VMWareVMsReport = @()
+$VMWareDisksReport = @()
+$VMWareNICsReport = @()
+
+foreach ($VM in $VMWareVMs) {
+    #Object for VM
+    $VMObject = New-Object -TypeName psobject
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "VMName" -Value $VM.Name
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "FQDN" -Value $VM.Guest.HostName
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "OperatingSystem" -Value $VM.Guest.OSFullName
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "PowerState" -Value $VM.PowerState
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "CPUs" -Value $VM.NumCpu
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "Memory" -Value $VM.MemoryGB
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "VMWareToolsStatus" -Value $VM.ExtensionData.Guest.ToolsStatus
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "VMWareToolsVersion" -Value $VM.ExtensionData.Guest.ToolsVersion
 
     #region: Disks
         #Get all disks
     $VMDisks = @()+ (Get-VMWareHardDisk -VM $VM)
         
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "NumberOfDisks" -Value $VMDisks.Count
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NumberOfDisks" -Value $VMDisks.Count
     if($VMDisks.Count -gt 0){    
             #Check for non 'flat' disks
         if(($VMDisks | select Disktype |Where-Object {$_.Disktype -ne "Flat"})) {$HasNonFlat = $true} else {$HasNonFlat = $false}
             
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "HasNonFlatDisks" -Value $HasNonFlat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "HasNonFlatDisks" -Value $HasNonFlat
                 
             #Concatenate disk capacities
         $DisksConcat = ""
-        $VMDisks  | foreach {$DisksConcat += "$([int] $_.CapacityGB);"}
+        $VMDisks  | foreach {
+            $DisksConcat += "$([int] $_.CapacityGB);"
+
+            $DiskObject = New-Object -TypeName psobject
+            Add-Member -InputObject $DiskObject -MemberType NoteProperty -Name "VMName" -Value $VM.Name
+            Add-Member -InputObject $DiskObject -MemberType NoteProperty -Name "DiskCapacityGB" -Value $_.CapacityGB
+            Add-Member -InputObject $DiskObject -MemberType NoteProperty -Name "DiskType" -Value $_.DiskType
+            Add-Member -InputObject $DiskObject -MemberType NoteProperty -Name "DiskFileName" -Value $_.Filename
+
+            $Datastore = Get-VMWareDatastore -Id ($_.ExtensionData.Backing.Datastore)
+            Add-Member -InputObject $DiskObject -MemberType NoteProperty -Name "DiskDataStore" -Value $Datastore.Name
+
+            $VMWareDisksReport += $DiskObject
+        }
         $DisksConcat = $DisksConcat.Substring(0,$DisksConcat.Length - 1)
 
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "DisksCapacityGB" -Value $DisksConcat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "DisksCapacityGB" -Value $DisksConcat
             #Calculate total capacity
         $TotalCapacity = [int]($VMDisks | Measure-Object -Property CapacityGB -sum).sum
             
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "TotalCapacityGB" -Value $TotalCapacity
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "TotalCapacityGB" -Value $TotalCapacity
     }
+
+    
+        
     #endregion
     
     #region: Network
         #Get all network adapters
     $NICs = @()+ ($VM | Get-VMWareNetworkAdapter)
-    Add-Member -InputObject $Object -MemberType NoteProperty -Name "NumberOfNICs" -Value $NICs.Count
+    Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NumberOfNICs" -Value $NICs.Count
         
         #Collect NIC info
     if($NICs.Count -gt 0){
@@ -49,19 +80,29 @@
             $NetworkNamesConcat += "$($_.NetworkName);"
             $NICTypeConcat += "$($_.Type);"
             $StartConnectedConcat += "$($_.ConnectionState.StartConnected);"
+
+            $NICsObject = New-Object -TypeName psobject
+            Add-Member -InputObject $NICsObject -MemberType NoteProperty -Name "VMName" -Value $VM.Name
+            Add-Member -InputObject $NICsObject -MemberType NoteProperty -Name "MACAddress" -Value $_.MacAddress
+            Add-Member -InputObject $NICsObject -MemberType NoteProperty -Name "NetworkName" -Value $_.NetworkName
+            Add-Member -InputObject $NICsObject -MemberType NoteProperty -Name "NICType" -Value $_.Type
+            Add-Member -InputObject $NICsObject -MemberType NoteProperty -Name "NICStartConnected" -Value $_.ConnectionState.StartConnected
+
+            $VMWareNICsReport += $NICsObject
         }
         $MACsConcat = $MACsConcat.Substring(0,$MACsConcat.Length - 1)
         $NetworkNamesConcat= $NetworkNamesConcat.Substring(0,$NetworkNamesConcat.Length - 1)
         $NICTypeConcat = $NICTypeConcat.Substring(0,$NICTypeConcat.Length - 1)
         $StartConnectedConcat = $StartConnectedConcat.Substring(0,$StartConnectedConcat.Length - 1)
 
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "NicMACAddress" -Value $MACsConcat
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "NicNetworkName" -Value $NetworkNamesConcat
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "NicType" -Value $NICTypeConcat
-        Add-Member -InputObject $Object -MemberType NoteProperty -Name "NicStartConnected" -Value $StartConnectedConcat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NicMACAddress" -Value $MACsConcat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NicNetworkName" -Value $NetworkNamesConcat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NicType" -Value $NICTypeConcat
+        Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "NicStartConnected" -Value $StartConnectedConcat
     }
-        
-        #IP Addresses
+    #endregion    
+    
+    #region: IP Addresses
         $IPs = $() + ($VM2.Guest.IPAddress)
         if($IPs.Count -gt 0){
             $IPv4Concat = ""
@@ -79,14 +120,27 @@
             $IPv4Concat = $IPv4Concat.Substring(0,$IPv4Concat.Length - 1)
             $Ipv6Concat = $Ipv6Concat.Substring(0,$Ipv6Concat.Length - 1)
 
-            Add-Member -InputObject $Object -MemberType NoteProperty -Name "IPv4Addresses" -Value $IPv4Concat
-            Add-Member -InputObject $Object -MemberType NoteProperty -Name "IPv6Addresses" -Value $Ipv6Concat
+            Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "IPv4Addresses" -Value $IPv4Concat
+            Add-Member -InputObject $VMObject -MemberType NoteProperty -Name "IPv6Addresses" -Value $Ipv6Concat
         }
     #endregion
 
-    $Object | Select-Object Name,FQDN,OperatingSystem,PowerState,CPUs,Memory,VMWareToolsStatus,VMWareToolsVersion,`
+    $VMWareVMsReport += $VMObject | Select-Object VMName,FQDN,OperatingSystem,PowerState,CPUs,Memory,VMWareToolsStatus,VMWareToolsVersion,`
                         NumberOfDisks,HasNonFlatDisks,DisksCapacityGB,TotalCapacityGB,`
                         NumberOfNICs,NicMACAddress,NicNetworkName,NicType,NicStartConnected,`
                         IPv4Addresses,IPv6Addresses
 } 
-$Report | ogv
+
+#region: Produce reports using grid view - this can be changed to export to CSV or any desired format
+    #All VMs
+$VMWareVMsReport |Select-Object VMName,FQDN,OperatingSystem,PowerState,CPUs,Memory,VMWareToolsStatus,VMWareToolsVersion,`
+                        NumberOfDisks,HasNonFlatDisks,DisksCapacityGB,TotalCapacityGB,`
+                        NumberOfNICs,NicMACAddress,NicNetworkName,NicType,NicStartConnected,`
+                        IPv4Addresses,IPv6Addresses | ogv -Title "VMWare VMs Report"6
+
+    #All Disks
+$VMWareDisksReport | Select-Object VMName, DiskCapacityGB, DiskType, DiskFileName, DiskDataStore | ogv -Title "VMWare Disks Report"
+
+    #All NICs
+$VMWareNICsReport | Select-Object VMName, MACAddress, NetworkName, NicType, NICStartConnected |ogv -Title "VMWare NICs Report"
+#endregion
